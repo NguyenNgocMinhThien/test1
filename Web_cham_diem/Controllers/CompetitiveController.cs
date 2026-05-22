c2f6aed97f773a25fc54f4badffacff718e0080c
NguyenNgocMinhThien <thiennnm22@uef.edu.vn>
2026-05-22 20:41:09 +0700

    update 1

---
+++ Web_cham_diem/Controllers/CompetitiveController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_cham_diem.Controllers
+{
+    public class CompetitiveController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
